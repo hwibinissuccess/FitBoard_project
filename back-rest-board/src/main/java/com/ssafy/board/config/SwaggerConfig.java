74b69511d98c0eec4f409c92d447710d4bd495bd
JungSeok <jungsuk7083@gmail.com>
2023-05-19 10:22:21 +0900

    `w123312

--- back-rest-board/src/main/java/com/ssafy/board/config/SwaggerConfig.java
+++ back-rest-board/src/main/java/com/ssafy/board/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.ssafy.board.controller"))
-				.paths(PathSelectors.ant("/api/**")).build().apiInfo(apiInfo());
+				.paths(PathSelectors.ant("/**")).build().apiInfo(apiInfo());
 	}
 
 	private ApiInfo apiInfo() {
