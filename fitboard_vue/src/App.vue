5db520b857b44a44f927537d1e8856a1445b58b2
hwibinissuccess <chlgnlqls3@nate.com>
2023-05-22 15:51:36 +0900

    hwibin vue one step

--- fitboard_vue/src/App.vue
+++ fitboard_vue/src/App.vue
@@ -1,88 +1,55 @@
 <template>
-  <div id="app">
-    <home-view v-if="welcome" @goMain="goMain()"></home-view>
-    <div v-else>
-      <header-nav></header-nav>
-      <div id="pageTop" />
-      <div>
-        <b-container class="mt-1">
-          <router-view />
-        </b-container>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
       </div>
-    </div>
-  </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HeaderNav from "@/components/common/HeaderNav.vue";
-import { mapState } from "vuex";
-import HomeView from "./views/HomeView.vue";
 
 export default {
-  name: "App",
-  components: {
-    HeaderNav,
-    HomeView,
-  },
-  data() {
-    return {
-      welcome: true,
-    };
-  },
-  methods: {
-    goMain() {
-      this.welcome = false;
-      this.$router.push("/video").catch(()=>{});
-    },
-    onScoll() {
-      const currentScrollPosition =
-        window.pageYOffset || document.documentElement.scrollTop;
-      if (currentScrollPosition < 0) return;
-      if (this.welcome) this.goMain();
-    },
-  },
-  computed: {
-    ...mapState(["logonUser"]),
-  },
-  mounted() {
-    window.addEventListener("scroll", this.onScoll);
-  },
-  beforeDestroy() {
-    window.removeEventListener("scroll", this.onScoll);
-  },
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-@import "/src/common/font/font.css";
-
-* {
-  font-family: "GangwonEduPowerExtraBoldA", Helvetica, Arial, sans-serif;  
-}
-
-#pageTop {
-  margin-top: 80px;
-}
-
-*::-webkit-scrollbar {
-  display: none; /* Chrome, Safari, Opera */
-}
-
-#app {
-  background-image: url("@/assets/logo2.png");
-  background-repeat: no-repeat;
-  background-size: 110%;
-  background-attachment: fixed;
-  background-position: bottom;
-  min-height: 980px;
-  height: 100%;
-}
-
-#app .nav-link {
-  color: #343a40;
-}
-
-#app .nav-link:active {
-  color: #596b86;
-}
-</style>
