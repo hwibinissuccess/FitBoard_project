e4dd26630d125b50b5f5b241546d6674ba9fd7f4
hwibinissuccess <chlgnlqls3@nate.com>
2023-05-22 15:43:03 +0900

    hwibin one step to vue

---
+++ fitboard_vue/src/components/common/HeaderNav.vue
@@ -0,0 +1,64 @@
+<template>
+    <header class="fixed-top">
+      <b-navbar type="dark" variant="secondary">
+        <div id="nav-container" class="container-fluid">
+          <div>
+            <b-navbar-brand :to="`/video`"
+              ><img id="logo" src="@/assets/logo2.png"
+            /></b-navbar-brand>
+          </div>
+          <div v-if="logonMember.userId != ''">
+            <b-nav pills>
+              <b-nav-item @click="logout">로그아웃</b-nav-item>
+              <b-nav-item :to="`/member/${logonUser.userId}`"
+                >마이페이지</b-nav-item
+              >
+            </b-nav>
+          </div>
+          <div v-else>  
+            <b-nav pills>
+              <b-nav-item :to="{ name: 'login' }">로그인</b-nav-item>
+              <b-nav-item :to="{ name: 'join' }">회원가입</b-nav-item>
+            </b-nav>
+          </div>
+        </div>
+      </b-navbar>
+    </header>
+  </template>
+  
+  <script>
+  import { mapState, mapMutations } from "vuex";
+  
+  export default {
+    name: "headerNav",
+    data() {
+      return {};
+    },
+    methods: {
+      ...mapMutations(["USER_LOGOUT"]),
+      logout() {
+        this.USER_LOGOUT();
+      },
+    },
+    computed: {
+      ...mapState(["logonUser"]),
+    },
+    mounted() {},
+  };
+  </script>
+  
+  <style scoped>
+  #logo {
+    height: 30px;
+  }
+  #nav-container .nav-item > a {
+    color: whitesmoke;
+  }
+  #nav-container .nav-item > a:hover {
+    color: rgb(141, 141, 141);
+  }
+  #nav-container .nav-item > a:active {
+    color: rgb(187, 188, 255);
+  }
+  </style>
+  
---
+++ fitboard_vue/src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="welPage">
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/wSQaGTRkxIA?autoplay=1&mute=1&rel=0&controls=0&modestbranding=1&amp;playlist=wSQaGTRkxIA&loop=1"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <span class="fixed-bottom d-flex justify-content-center">
+        <!-- <img src="@/assets/scroll.gif" /> -->
+        <img id="scrimg" src="@/assets/logo2.png" @click="goMain()"/>
+      </span>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "homeView",
+    data() {
+      return {};
+    },
+    methods: {
+      goMain(){
+        this.$emit("goMain")
+      }
+    },
+  };
+  </script>
+  
+  <style>
+  #welPage {
+    height: 100vh; /* 넓이 값은 조정해서 사용 */
+    width: 100vw;
+  }
+  #scrimg {
+    width: 33vw;
+    z-index: 99;
+    animation: watch 1s linear infinite both;
+  }
+  
+  
+  @keyframes watch {
+    0% {
+      transform: translate(0);
+    }
+    50% {
+      transform: translate(0, -30px);
+    }
+    100% {
+      transform: translate(0);
+    }
+  }
+  #scrimg:hover {
+    animation-play-state: paused;
+  }
+  </style>
